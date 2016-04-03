6fdda141729d1d8539171e3a1ff1223fb75b28d4
jpsiyu <jpsiyu@163.com>
2016-04-04 00:48:52 +0800

    add

---
+++ Assets/Scripts/CfgTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CfgTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("Test", 2);
+	}
+	
+	void Test () {
+        CfgCtrlHelper h = new CfgCtrlHelper();
+        Debug.Log("OK");
+	}
+}
