8b5b1ed923324cfec2f2718491af293c8c838ed0
jpsiyu <jpsiyu@163.com>
2016-04-03 22:35:58 +0800

    rename them

---
+++ Assets/Scripts/CfgCtrl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CfgCtrl : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("Test", 3f);
+    }
+
+    void Test()
+    {
+        Debug.Log("Start a Test ... ");
+
+        string path = string.Format("{0}/../CSV", Application.dataPath);
+        string fileName = "copy.csv";
+        CfgUtility<CopyCfg> utiliy = new CfgUtility<CopyCfg>();
+        List<string[]> list = utiliy.FileToList(path, fileName);
+
+        List<Hashtable> tableList = utiliy.ListToHashtable(list);
+
+        Dictionary<int, CopyCfg> dict = utiliy.TableListToDict(tableList);
+
+        Debug.Log("Test finish!");
+    }
+
+
+}
---
+++ Assets/Scripts/CopyCfg.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CopyCfg : ICfg
+{
+
+    private int id;
+    private string desc;
+    private int lockLv;
+    private int cost;
+    private int reward;
+    private int cd;
+
+    public int GetId()
+    {
+        return Id;
+    }
+
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    public string Desc
+    {
+        get { return desc; }
+        set { desc = value; }
+    }
+
+    public int LockLv
+    {
+        get { return lockLv; }
+        set { lockLv = value; }
+    }
+
+    public int Cost
+    {
+        get { return cost; }
+        set { cost = value; }
+    }
+
+    public int Reward
+    {
+        get { return reward; }
+        set { reward = value; }
+    }
+
+    public int Cd
+    {
+        get { return cd; }
+        set { cd = value; }
+    }
+}
