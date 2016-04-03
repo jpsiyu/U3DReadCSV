293b6d44d69f0ac3b383bb9497ff14dea13e4951
jpsiyu <jpsiyu@163.com>
2016-04-03 16:01:50 +0800

    test ok

---
+++ Assets/Scripts/Way01/FileController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FileController : MonoBehaviour {
+
+    private string path;
+    private string fileName;
+
+	// Use this for initialization
+	void Start () {
+
+        path = string.Format("{0}/../CSV", Application.dataPath);
+        fileName = "copy.csv";
+
+        Way01CSV.GetInstance().LoadFile(path, fileName);
+
+        foreach (string s in Way01CSV.GetInstance().ArrayData) {
+            Debug.Log(s);
+        }
+	}
+	
+
+}
--- Assets/Scripts/Way01/Way01CSV.cs
+++ Assets/Scripts/Way01/Way01CSV.cs
@@ -1,15 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using System;
 
-public class Way01CSV : MonoBehaviour {
+public class Way01CSV  {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private static Way01CSV csv;
+    private List<string> m_ArrayData;
+
+    public static Way01CSV GetInstance() {
+        if (csv == null) {
+            csv = new Way01CSV(); 
+        }
+        return csv;
+    }
+
+    public List<string> ArrayData {
+        get { return m_ArrayData; }
+    }
+
+    private Way01CSV() { m_ArrayData = new List<string>(); }
+
+    public void LoadFile(string path, string fileName) {
+        m_ArrayData.Clear();
+        StreamReader sr = null;
+        string filePath = string.Format("{0}/{1}", path, fileName);
+        try {
+            sr = File.OpenText(filePath);
+        }
+        catch (Exception e){
+            Debug.LogError("CSV read failed! " + filePath + e.Message);
+            return;
+        }
+        string line;
+        while ((line = sr.ReadLine()) != null) {
+            m_ArrayData.Add(line);
+        }
+        sr.Close();
+        sr.Dispose();
+    }
+
 }
